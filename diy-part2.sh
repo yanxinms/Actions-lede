9945b7b2e74224721374d545f0b9f702ea4b79f3
yanxinms <yanxinms@gmail.com>
2026-07-02 21:05:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,65 @@ git clone --depth=1 --single-branch --branch master https://github.com/vernesong
 mv openclash_repo/luci-app-openclash package/
 rm -rf openclash_repo
 
+rm -f feeds/qmodem/application/qmodem/Makefile
+
+# 直接将新的内容写入目标路径
+cat << 'EOF' > feeds/qmodem/application/qmodem/Makefile
+include $(TOPDIR)/rules.mk
+include $(INCLUDE_DIR)/kernel.mk
+
+# Include unified version
+include ../../version.mk
+
+PKG_NAME:=qmodem
+PKG_RELEASE:=$(QMODEM_RELEASE)
+PKG_VERSION:=$(QMODEM_VERSION)
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/$(PKG_NAME)
+  SECTION:=utils
+  CATEGORY:=Utilities
+  TITLE:=QModem scripts
+  DEPENDS:= \
+        +kmod-usb2 +kmod-usb3 \
+        +kmod-usb-serial +kmod-usb-serial-option +kmod-usb-serial-qualcomm \
+        +kmod-usb-net +kmod-usb-acm \
+        +kmod-usb-wdm \
+        +kmod-usb-net-cdc-ether \
+        +kmod-usb-net-cdc-mbim \
+        +kmod-usb-net-rndis \
+        +kmod-usb-net-cdc-ncm +kmod-usb-net-huawei-cdc-ncm \
+        +ubus-at-daemon +tom_modem +terminfo +sms-tool_q \
+        +modem_scan \
+        +jq +bc\
+        +coreutils +coreutils-stat \
+        +usbutils \
+        +PACKAGE_luci-app-qmodem_INCLUDE_rdisc6:rdisc6 \
+        +PACKAGE_luci-app-qmodem_INCLUDE_ndisc6:ndisc6 \
+        +PACKAGE_luci-app-qmodem_INCLUDE_ADD_PCI_SUPPORT:pciutils \
+        +PACKAGE_luci-app-qmodem_INCLUDE_ADD_MTK_T7XX_SUPPORT:umbim
+endef
+
+define Package/$(PKG_NAME)/description
+	QModem scripts
+endef
+
+define Build/Prepare
+endef
+
+define Build/Compile
+endef
+
+define Package/$(PKG_NAME)/install
+	$(CP) ./files/* $(1)/
+endef
+
+$(eval $(call BuildPackage,$(PKG_NAME)))
+EOF
+
+echo "qmodem Makefile 替换成功！"
+
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
